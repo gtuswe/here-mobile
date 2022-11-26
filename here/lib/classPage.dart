6f899aa67d4d5f0d488624b11e15233a824bb422
Tefoni <atacanbasaran74@gmail.com>
2022-11-26 10:42:37 +0300

    classPage developing

--- here/lib/classPage.dart
+++ here/lib/classPage.dart
@@ -10,6 +10,48 @@ class ClassPage extends StatefulWidget {
 class _ClassPageState extends State<ClassPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Software Engineering")),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(onPressed: (() {}), icon: const Icon(Icons.more_vert)))
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: Card(
+                child: SizedBox(
+              height: 200,
+              child: Container(color: Colors.red),
+            )),
+          ),
+          Expanded(
+            flex: 2,
+            child: DefaultTabController(
+              length: 2,
+              child: TabBar(tabs: [
+                Text(
+                  "Attendance",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Text(
+                  "Participants",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ]),
+            ),
+          ),
+          const Expanded(
+              flex: 12,
+              child: TabBarView(
+                children: [Text("123"), Text("345")],
+              ))
+        ],
+      ),
+    );
   }
 }
