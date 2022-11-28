2df7c78174bd0eba243d39d886c78003c621f8d3
Halil I. ILHAN <hybrayhem@gmail.com>
2022-11-28 22:30:21 +0300

    create main page and navigations

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Center(
+      child: Text('Home!'),
+    );
   }
 }
---
+++ lib/view/main_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:here/view/calendar/calendar_page.dart';
+import 'package:here/view/home/home_page.dart';
+import 'package:here/view/profile/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      bottomNavigationBar: _bottomBar,
+      body: SafeArea(
+        child: IndexedStack(
+          index: _pageIndex,
+          children: const <Widget>[
+            HomePage(),
+            CalendarPage(),
+            Center(child: Text("No notifications.")),
+            ProfilePage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar get _appBar => AppBar(
+        title: const Text('Here!'),
+        centerTitle: true,
+      );
+
+  void _onTapBottomBar(int value) {
+    setState(() {
+      _pageIndex = value;
+    });
+  }
+
+  Widget get _bottomBar => NavigationBar(
+        selectedIndex: _pageIndex,
+        onDestinationSelected: _onTapBottomBar,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home_filled), label: "Home"),
+          NavigationDestination(
+              icon: Icon(Icons.calendar_month), label: "Calendar"),
+          NavigationDestination(
+              icon: Icon(Icons.notifications), label: "Activity"),
+          NavigationDestination(
+              icon: Icon(Icons.person_rounded), label: "Profile"),
+        ],
+      );
+}
